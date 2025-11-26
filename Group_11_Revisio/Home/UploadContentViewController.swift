84ac5e298f8920200ad4fdcdf7f41435fc3face0
Mithil jagtap <mithiljagtap123@gmail.com>
2025-11-26 14:24:13 +0530

    updated

---
+++ Group_11_Revisio/Home/UploadContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UploadContentViewController.swift
+//  Group_11_Revisio
+//
+//  Created by Mithil on 26/11/25.
+//
+
+import UIKit
+
+class UploadContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
